e8390521ab198a203e3fc538e28f3c4d71c664a8
Corentin <corentin.milot@free.fr>
2018-06-14 14:39:13 +0200

    Ajout de br

--- src/index.php
+++ src/index.php
@@ -87,12 +87,12 @@ else {
     <div class="hidden" id="formations">
     <ul>
     <div class="hoverme">  <li>2017 - <strong>Bac STIDD - Lycée Louis Couffignal</strong> - Strasbourg</li><br />
-      <img class="imagelycee" src="img/logolycéecouff.jpg" alt="Logo Lycée" title="Le logo du lycée !" style="width: 850px;">
-      <img class="imagelycee" src="img/lycéecouff.jpg" alt="Lycée" title="Le lycée !" style="width: 530px;">
+      <img class="imagelycee" src="img/logolyceecouff.jpg" alt="Logo Lycée" title="Le logo du lycée !" style="width: 850px;">
+      <img class="imagelycee" src="img/lyceecouff.jpg" alt="Lycée" title="Le lycée !" style="width: 530px;">
     </div><br />
-		<div class="hoverme2"> <li style="margin-right: 10px;">2013 - <b>Brevet des collèges - Collège des Roseaux</b> - Illkirch</li>
+		<div class="hoverme2"> <li style="margin-right: 10px;">2013 - <b>Brevet des collèges - Collège des Roseaux</b> - Illkirch</li><br />
 <img class="imagecollege" src="img/college.jpg" alt="Collège" title="Le collège !" style="width: 530px; margin-right: 20px;">
-<img class="imagecollege" src="img/roseaux.jpg" alt="Collège" title="Panneau du collège !" style="width: 530px; margin-left: 20px;">
+<img class="imagecollege" src="img/roseaux.jpg" alt="Collège" title="Panneau du collège !" style="width: 530px; margin-left: 20px;"><br /><br />
     </div>
 	  </ul>
     </div>
