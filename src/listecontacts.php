2dd715dce69bd5ae34f0bca05c8060cc7b4d6f50
Corentin <corentin.milot@free.fr>
2018-06-14 16:36:42 +0200

    Création du tableau

---
+++ src/listecontacts.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1"/>
+        <title>Liste de mes contacts</title>
+  </head>
+
+  <body>
+
+    <table>
+
+       <caption>Passagers du vol 377</caption>
+
+          <tr>
+
+            <th>Nom et/ou nom de l'entreprise</th>
+
+            <th>Adresse</th>
+
+            <th>Téléphone</th>
+
+            <th> Date</th>
+
+            <th>Email</th>
+
+          </tr>
+
+        
+
+    </table>
+
+  </body>
+</html>
