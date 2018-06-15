a2acded67bfab4b167c004a5d1b994d7e59f0d23
Corentin <corentin.milot@free.fr>
2018-06-15 16:12:56 +0200

    Page CSS complétée

--- Pages/css.php
+++ Pages/css.php
@@ -2,12 +2,21 @@
 <html>
   <head>
     <meta charset="utf-8" />
+      <link rel="stylesheet" href="stylecss.css" />
 
     <title>Le CSS</title>
-
 </head>
-
+    <h2>Courte présentation du CSS</h2>
 <body>
+        <p> Le CSS est un autre language web, permettant lui de structurer page web.<br />
+          Seul, il n'est pas exploitable et s'utile avec le HTML, principalement pour la mise en forme de la page.<br />
+          Il permet entre autre de mettre un fond, des couleurs sur les textes et liens, de centrer et d'organier son texte...<br />
+          Il est même utilisé pour creer des animatins simples, tel que celles sur les icones que vous avez vu précédemment.<br />
+          Cette page est donc faite avec du HMTL et du CSS</p>
 
+        <p>Cliquez <a href="php.php">ici</a> pour acceder a l'explication du PHP, et <a href="../src/index.php">ici<a/> pour revenir au Curriculum Vitae.</p>
+<br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br />
+<br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br />
+<br /><br /><br />
 
 </body>
