805f657ee7ef8e1bdd4806ac2aa525c82065c32a
Corentin <corentin.milot@free.fr>
2018-06-15 15:43:04 +0200

    Page HTML complétée

--- Pages/html.php
+++ Pages/html.php
@@ -5,9 +5,19 @@
 
     <title>Le HTML</title>
 
+      <h2>Courte présentation du HTML</h2>
+
+          <p> Le HTML est un language web permettant de creer la base d'une page web.<br />
+            Seul, il permet principalement de mettre les informations et le fond de la page.<br />
+            La mise en page se fait via un autre language, bien que le HTML puisse en<br />
+            faire un petit peu (des tableaux par exemple), il reste très limité et peu pratique.<br />
+            Cette page est entièrement faite en HTML</p>
+
+          <p>Cliquez <a href="css.php">ici</a> pour acceder a l'explication du CSS, et <a href="../src/index.php">ici<a/> pour revenir au Curriculum Vitae.</p>
+
 </head>
 
 <body>
 
-  
+
 </body>
